539479562ce3977aa656e164c07be920cc563512
Bry Cox <carl.cc@gmail.com>
2019-08-10 17:06:57 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,6 +14,12 @@ class SongsController < ApplicationController
     redirect_to song_path(@song)
   end
 
+  def update
+    @song = Song.find(params[:id])
+    @song.update(params.require(:song).permit(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
+  end
+
   def edit
     @song = Song.find(params[:id])
   end
